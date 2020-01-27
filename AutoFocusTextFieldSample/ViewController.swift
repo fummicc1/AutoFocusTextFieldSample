2dda142aeec9d32270db5e39dffcc48cac43a36b
fummicc1 <fumiya989@gmail.com>
2020-01-27 17:36:59 +0900

    インデント修正

--- AutoFocusTextFieldSample/ViewController.swift
+++ AutoFocusTextFieldSample/ViewController.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var firstTextField: UITextField!
-    @IBOutlet var secondTextField: UITextField!
-    @IBOutlet var thirdTextField: UITextField!
-    @IBOutlet var fourthTextField: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        firstTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
-        secondTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
-        thirdTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
-        fourthTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
-    }
-    
+	
+	@IBOutlet var firstTextField: UITextField!
+	@IBOutlet var secondTextField: UITextField!
+	@IBOutlet var thirdTextField: UITextField!
+	@IBOutlet var fourthTextField: UITextField!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		firstTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
+		secondTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
+		thirdTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
+		fourthTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
+	}
+	
 	@objc func textDidChange(_ textField: UITextField) {
 		
 		// 入力した文字を変数に格納
@@ -32,48 +32,48 @@ class ViewController: UIViewController {
 		var isBack: Bool = false
 		
 		// 空だったら、１つ前に戻る
-        if text.isEmpty {
+		if text.isEmpty {
 			// 前に戻るように設定する
 			isBack = true
 		}
-        
-        switch textField {
+		
+		switch textField {
 			
-        case firstTextField:
-            
+		case firstTextField:
+			
 			// 前に戻るか次に進むかをif文で確認する
-            if isBack == false {
-                secondTextField.becomeFirstResponder()
-            }
-            
-        case secondTextField:
-            
+			if isBack == false {
+				secondTextField.becomeFirstResponder()
+			}
+			
+		case secondTextField:
+			
 			// 前に戻るか次に進むかをif文で確認する
-            if isBack == true {
-                firstTextField.becomeFirstResponder()
-            } else {
-                thirdTextField.becomeFirstResponder()
-            }
-            
-        case thirdTextField:
-            
+			if isBack == true {
+				firstTextField.becomeFirstResponder()
+			} else {
+				thirdTextField.becomeFirstResponder()
+			}
+			
+		case thirdTextField:
+			
 			// 前に戻るか次に進むかをif文で確認する
-            if isBack == true {
-                secondTextField.becomeFirstResponder()
-            } else {
-                fourthTextField.becomeFirstResponder()
-            }
-            
-        case fourthTextField:
-            
+			if isBack == true {
+				secondTextField.becomeFirstResponder()
+			} else {
+				fourthTextField.becomeFirstResponder()
+			}
+			
+		case fourthTextField:
+			
 			// 前に戻るか次に進むかをif文で確認する
 			if isBack == true {
 				thirdTextField.becomeFirstResponder()
 			} else {
 				fourthTextField.resignFirstResponder()
 			}
-        default:
-            break
-        }
-    }
+		default:
+			break
+		}
+	}
 }
